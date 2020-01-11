a36bce0a0d5ff1eb64789c1f3aa7f90ce1134f85
Kembek <zevs.312.777@gmail.com>
2020-01-11 22:58:07 +0600

    Init: ng-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+
+export interface Post {
+  title: string;
+  text: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-examples';
+  e: number = Math.E;
+
+  str: string = "hello world";
+
+  date: Date = new Date();
+
+  float: number = 0.55;
+
+  search: string = "";
+
+  field: string = "title";
+
+  promise: Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Promise result");
+    }, 4000);
+  });
+
+  date$: Observable<Date> = new Observable(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+
+  obj = {
+    a: 1,
+    b: [2, 3, 4],
+    c: {
+      d: 5,
+      e: {
+        j: {
+          s: 6,
+          w: 7
+        }
+      }
+    }
+  };
+
+  num: number = 3;
+
+  posts: Post[] = [
+    {
+      title: "post one",
+      text:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, enim!"
+    },
+    {
+      title: "post second",
+      text:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, enim!"
+    },
+    {
+      title: "post third",
+      text: "фыпф фпвы asdg adsgjar"
+    }
+  ];
+
+  addPost() {
+    this.posts.unshift({
+      title: "Beknazar",
+      text: "Hello my name is Beknazar"
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MultByPipe } from "./pipes/mult-by.pipe";
+import { ExMarksPipe } from "./pipes/ex-marks.pipe";
+import { FilterPipe } from "./pipes/filter.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
