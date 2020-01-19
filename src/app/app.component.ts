3246216e626c2cfdae7326c02439b54af9fb5640
Kembek <zevs.312.777@gmail.com>
2020-01-19 13:46:35 +0600

    update: form was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'ng-form';
+export class AppComponent implements OnInit {
+  regForm: FormGroup;
+
+  ngOnInit() {
+    this.regForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    });
+  }
+
+  submit() {
+    const regFormData = this.regForm.value;
+
+    console.log("RegForm", this.regForm);
+    console.log("RegForm data", regFormData);
+  }
 }
