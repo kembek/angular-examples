bd714914aeef6672eed2f26fba4605e5fb11a21d
Kembek <zevs.312.777@gmail.com>
2020-03-17 19:43:15 +0600

    init: the module tutorial was began

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {HomePageComponent} from './home-page/home-page.component'
+import {AboutPageComponent} from './about-page/about-page.component'
+import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: 'about', component: AboutPageComponent, children: [
+      {path: 'extra', component: AboutExtraPageComponent}
+    ]},
+    {path: '', component: HomePageComponent, pathMatch: 'full'}
+  ])],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
