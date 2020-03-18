ec59b86ab703495921a00b229d99e67ad48f5dd0
Kembek <zevs.312.777@gmail.com>
2020-03-18 14:18:56 +0600

    update: event emitter was tested

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,11 @@
 import { CounterComponent } from "./counter.component";
+import { FormBuilder } from "@angular/forms";
 
 describe("CounterComponent", () => {
   let component: CounterComponent;
 
   beforeEach(() => {
-    component = new CounterComponent();
+    component = new CounterComponent(new FormBuilder());
   });
 
   it("should increment counter by 1", () => {
@@ -21,5 +22,39 @@ describe("CounterComponent", () => {
     // TODO: set 4 for began this number
     component.increment();
     expect(component.counter).toBe(5);
+  });
+
+  it("should inceremnt value by event emitter", () => {
+    let result = null;
+
+    component.counterEmitter.subscribe(v => (result = v));
+    component.increment();
+
+    expect(component.counter).toBe(1);
+  });
+
+  it("should decremnt value by event emitter", () => {
+    let result = null;
+
+    component.counterEmitter.subscribe(v => (result = v));
+    component.decrement();
+
+    expect(component.counter).toBe(-1);
+  });
+
+  it("should create form with two controls", () => {
+    const controls = ["username", "password"];
+
+    controls.forEach(f => {
+      expect(component.counterForm.contains(f)).toBeTruthy();
+    });
+  });
+
+  it("should mark login as invalid if empty value", () => {
+    const control = component.counterForm.get("username");
+
+    control.setValue("");
+
+    expect(control.valid).toBeFalsy();
   });
 });
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,19 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-counter",
   template: `
-    Counter: {{ counter }}
+    <p>Counter: {{ counter }}</p>
+    <div>
+      <button (click)="increment()">Increment</button>
+      <button (click)="decrement()">Decrement</button>
+    </div>
   `
 })
 export class CounterComponent {
   counter = 0;
+  counterForm: FormGroup;
+
+  @Output() counterEmitter = new EventEmitter<number>();
+
+  constructor(private fb: FormBuilder) {
+    this.counterForm = this.fb.group({
+      username: this.fb.control("", [Validators.required]),
+      password: this.fb.control("", [
+        Validators.required,
+        Validators.minLength(5)
+      ])
+    });
+  }
 
   decrement() {
-    this.counter = --this.counter;
+    --this.counter;
+    this.counterEmitter.emit(this.counter);
   }
 
   increment() {
-    this.counter = ++this.counter;
+    ++this.counter;
+    this.counterEmitter.emit(this.counter);
   }
 }
