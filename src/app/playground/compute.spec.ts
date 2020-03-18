73681caaa054b6cf699cb5edcf2173c8a1be22d8
Kembek <zevs.312.777@gmail.com>
2020-03-18 11:48:36 +0600

    add: the first lesson has been added

---
+++ src/app/playground/compute.spec.ts
@@ -0,0 +1,15 @@
+import { compute } from "./compute";
+
+describe("compute function", () => {
+  it("should return zero for negative number", () => {
+    const result = compute(-1);
+
+    expect(result).toBe(0);
+  });
+
+  it("should increment positive number", () => {
+    const result = compute(1);
+
+    expect(result).toBe(2);
+  });
+});
