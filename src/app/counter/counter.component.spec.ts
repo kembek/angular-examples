d4375cf685980926f3ecd0e5693100fb1ee6b9de
Kembek <zevs.312.777@gmail.com>
2020-03-19 12:47:30 +0600

    Update counter.component.spec.ts

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,51 @@
-import {CounterComponent} from "./counter.component";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { CounterComponent } from "./counter.component";
+import { By } from "@angular/platform-browser";
 
-describe('CounterComponent', () => {
-  let component: CounterComponent
+describe("CounterComponent", () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
 
   beforeEach(() => {
-    component = new CounterComponent()
-  })
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    });
 
-})
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+  });
+
+  it("should be created", () => {
+    expect(component).toBeDefined();
+  });
+
+  it("should change the property in the component", () => {
+    const count = 42;
+
+    component.counter = count;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css(".counter"));
+    let element: HTMLElement = debugElement.nativeElement;
+
+    expect(element.textContent).toContain(count.toString());
+  });
+
+  it("should add the green class in the element if count is even number", () => {
+    const evenCount = 6;
+
+    component.counter = evenCount;
+    fixture.detectChanges();
+    let debugElement = fixture.debugElement.query(By.css(".counter"));
+    let element: HTMLElement = debugElement.nativeElement;
+
+    expect(element.classList.contains("green")).toBeTruthy();
+  });
+
+  it("should change if button clicked", () => {
+    let btn = fixture.debugElement.query(By.css("#increment"));
+    btn.triggerEventHandler("click", null);
+
+    expect(component.counter).toBe(1);
+  });
+});
