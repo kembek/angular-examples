902a5b399fc90fa8d9b9478629cee3208a4b2f4a
Kembek <zevs.312.777@gmail.com>
2020-03-19 12:57:58 +0600

    Update posts.component.spec.ts

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,13 +1,32 @@
-import {PostsComponent} from "./posts.component";
-import {PostsService} from "./posts.service";
+import { PostsComponent } from "./posts.component";
+import { PostsService } from "./posts.service";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { of } from "rxjs";
 
-describe('PostsComponent', () => {
-  let component: PostsComponent
-  let service: PostsService
+describe("PostsComponent", () => {
+  let fixture: ComponentFixture<PostsComponent>;
+  let component: PostsComponent;
+  let service: PostsService;
 
   beforeEach(() => {
-    service = new PostsService(null)
-    component = new PostsComponent(service)
-  })
+    TestBed.configureTestingModule({
+      declarations: [PostsComponent],
+      imports: [HttpClientModule],
+      providers: [PostsService]
+    });
+    fixture = TestBed.createComponent(PostsComponent);
+    component = fixture.componentInstance;
+    service = TestBed.get(PostsService);
+  });
 
-})
+  it("should call fetch in the ngOnInit lificycle hook", () => {
+    const posts = [1, 2, 3, 4, 5];
+
+    spyOn(service, "fetch").and.returnValue(of(posts));
+
+    fixture.detectChanges();
+
+    expect(component.posts).toEqual(posts);
+  });
+});
